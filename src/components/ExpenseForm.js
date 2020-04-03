02197b43c9bf4bf814f5c58572b91b0afbdb6394
John Doe <nadia643@gmail.com>
2020-04-03 17:36:16 +0100

    all components created as functional

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ExpenseForm = () => {
+    return (
+        <div>
+            hello from expense form
+        </div>
+    )
+}
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ExpenseItem = () => {
+    return (
+        <div>
+            hello from expense item
+        </div>
+    )
+}
